a56248283616ece9b6d90e1c32ec7d47eead80f1
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2020-01-25 15:45:57 +0530

    linkedlist pop

--- DS-Algo/linkedlist.js
+++ DS-Algo/linkedlist.js
@@ -23,6 +23,25 @@ class LinkedList {
         this.length++;
         return this;
     }
+    pop() {
+        if(!this.head) {
+            return undefined
+        }
+        let current = this.head
+        let pre = current;
+        while(current.next) {
+            pre = current
+            current = current.next
+        }
+        this.tail = pre
+        this.tail.next = null
+        this.length-- 
+        if(this.length === 0){
+            this.head = null
+            this.tail = null
+        }
+        return current;
+    }
 }
 
 let list = new LinkedList()
@@ -30,5 +49,11 @@ list.push("hello");
 list.push("there");
 list.push("how");
 list.push("are");
-console.log(list.head.next)
+//console.log(list.head.next)
+console.log(list);
+list.pop();
+list.pop();
+list.pop();
+//list.pop();
+//list.pop();
 console.log(list);
