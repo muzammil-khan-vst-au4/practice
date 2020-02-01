26e81699786f8fe5527471eb2ec73df4f01676d1
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2020-02-01 14:53:43 +0530

    Updates

---
+++ precache-manifest.d6f63894ac4af015d255d439f7ced37d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95914287633ef831b820cbeb3aace4be",
+    "url": "/myreactapp/index.html"
+  },
+  {
+    "revision": "a9a4ce3b1e8386edb1a2",
+    "url": "/myreactapp/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "090ceff14c174f1b504e",
+    "url": "/myreactapp/static/js/2.5f0676f3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myreactapp/static/js/2.5f0676f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9a4ce3b1e8386edb1a2",
+    "url": "/myreactapp/static/js/main.05c82155.chunk.js"
+  },
+  {
+    "revision": "23e9f023d261a8035336",
+    "url": "/myreactapp/static/js/runtime-main.20c4b5bb.js"
+  }
+]);
